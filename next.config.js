f3f05007a586c439ec0f49f9fbe48376c22ef0b9
RogelioLB <rogelio20052011@gmail.com>
2021-10-17 20:00:43 -0600

    🐛 RootProject

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images:{
     domains:["i.ibb.co"]
+  },
+  serverRuntimeConfig: {
+      PROJECT_ROOT: __dirname
   }
 }
--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,13 +1,16 @@
 import fs from "fs";
 import path from "path";
+import getConfig from "next/config"
+const {serverRuntimeConfig} = getConfig()
 
 export default (req,res)=>{
-    const dirname = path.resolve(__dirname,"../../../../projects")
+    const dirname = path.resolve(serverRuntimeConfig.PROJECT_ROOT,"./projects")
     const directories = fs.readdirSync(dirname);
     const data = directories.map((el)=>{
         try{
             const data = fs.readFileSync(path.resolve(dirname,el,"data.json")).toString();
             const json = JSON.parse(data);
+            console.log(json)
             return json
         }catch(err){
             return {err}
