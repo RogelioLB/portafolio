726431a5c0743568f0af112f2fa4c91b6c8092e0
RogelioLB <rogelio20052011@gmail.com>
2021-10-17 20:12:02 -0600

    Test

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -4,13 +4,13 @@ import getConfig from "next/config"
 const {serverRuntimeConfig} = getConfig()
 
 export default (req,res)=>{
+    console.log(serverRuntimeConfig.PROJECT_ROOT)
     const dirname = path.resolve(serverRuntimeConfig.PROJECT_ROOT,"./projects")
     const directories = fs.readdirSync(dirname);
     const data = directories.map((el)=>{
         try{
             const data = fs.readFileSync(path.resolve(dirname,el,"data.json")).toString();
             const json = JSON.parse(data);
-            console.log(json)
             return json
         }catch(err){
             return {err}
